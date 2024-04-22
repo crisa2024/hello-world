67fa22779a82b28f3cd50d7163f58114c34e1202
Cristina <cavolfiore199922@gmail.com>
2024-04-22 22:59:20 +0100

    Added comments to the file hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
-
+# Asking the user to enter their name and assigning it to the variable 'name':
 name = input("\nPlease enter your name: ")
+# Asking the user to enter their surname and assigning it to the variable 'surname': 
 surname = input("\nPlease enter your surname: ")
+# Printing out the following message:
 print(f"Hello World! I am {name} {surname}.\nI'm learning Python!")
